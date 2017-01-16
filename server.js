b8fb6b272a74832932a1a99802184e7fac8a89ad
Krzysztof Miąsko <kmiasko@gmail.com>
2017-01-16 17:17:31 +0000

    first lines

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const moment = require('moment');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+  res.end();
+});
+
+app.listen(process.env.PORT || 8080):
